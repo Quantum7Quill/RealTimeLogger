d56f118ed45d0465cc400bef3e3dde1384816b2d
Vivek Kumar <kumar.vivekdv@gmail.com>
2024-01-31 03:37:35 +0530

    extended utility to support updates

--- aggregate_events.py
+++ aggregate_events.py
@@ -3,14 +3,42 @@
 import json
 from datetime import datetime, timedelta
 import argparse
+import os
 
 def read_events_from_file(file_path):
     with open(file_path, 'r') as file:
         events = json.load(file)
     return events
 
-def aggregate_events(events):
+def aggregate_events(existing_data, events):
+    
+    # aggregated data is event data grouped by user_id and date
     aggregated_data = {}
+
+    # if existing data is present, then add it to aggregated_data to handle updates
+    if len(existing_data) > 0:
+        for event in existing_data:
+            user_id = event['userId']
+            date = event['date']
+
+            count_by_event_type ={}
+            for key, value in event.items():
+                if key not in ['userId', 'date']:
+                    count_by_event_type[key] = value
+
+            if user_id not in aggregated_data:
+                aggregated_data[user_id] = {}
+
+            if date not in aggregated_data[user_id]:
+                aggregated_data[user_id][date] = {}
+
+            for event_type, count in count_by_event_type.items():
+                if event_type not in aggregated_data[user_id][date]:
+                    aggregated_data[user_id][date][event_type] = 0
+
+                aggregated_data[user_id][date][event_type] += count
+
+    # add new events to aggregated_data
     for event in events:
         user_id = event['userId']
         timestamp = event['timestamp']
@@ -59,12 +87,18 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input_file", required=True)
     parser.add_argument("-o", "--output_file", required=True)
+    parser.add_argument("--update", action="store_true")
+
 
     args = parser.parse_args()
+    existing_data = []
+    if args.update and os.path.exists(args.output_file):
+        with open(args.output_file, 'r') as file:
+            existing_data = json.load(file)
 
     try:
-        events = read_events_from_file(args.input_file)
-        daily_summary = aggregate_events(events)
+        new_events = read_events_from_file(args.input_file)
+        daily_summary = aggregate_events(existing_data, new_events)
         write_daily_summary_to_file(daily_summary, args.output_file)
         print(f"Daily summary written to {args.output_file}")
 
